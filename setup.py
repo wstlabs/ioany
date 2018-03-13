490454f55fbca2f39ea12491b487e68701b21a8e
wstlabs <notyet@notyet.org>
2018-03-13 16:33:57 -0400

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "ioany",
-    version = "0.0.2",
+    version = "0.0.4",
     author = "wstlabs",
     author_email = "wst@pobox.com",
     description = ("Painless slurping and conversion between CSV, JSON and row/record formats"),
@@ -14,7 +14,7 @@ setup(
     keywords = "dataframe csv json iterable stream-oriented",
     url = "http://packages.python.org/ioany",
     packages= ['ioany', 'ioany.util', 'tests'],
-    long_description=read('README.rst'),
+    long_description=read('README.md'),
     classifiers= [
         "Topic :: Utilities",
         "Development Status :: 2 - Pre-Alpha",
