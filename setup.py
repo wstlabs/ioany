8fb3e81735adbddbbcb97b56c41c8b1c345ee86a
wstlabs <notyet@notyet.org>
2018-09-26 13:59:36 +0200

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "ioany",
-    version = "0.0.7",
+    version = "0.0.8",
     author = "wstlabs",
     author_email = "wst@pobox.com",
     description = ("Painless slurping and conversion between CSV, JSON and row/record formats"),
